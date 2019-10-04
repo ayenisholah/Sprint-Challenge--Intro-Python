8bfb3c0c791ce3e1980b63226338c3f1697d99dc
Shola Ayeni <ayenisholah@yahoo.com>
2019-10-04 10:39:11 +0100

    OOP

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -9,6 +9,40 @@
 # [Car]  [Motorcycle]
 #
 # Each class can simply "pass" for its body. The exercise is about setting up
+
+class Vehicle:
+  def __init__(self):
+    pass 
+
+class FlightVehicle(Vehicle):
+  def __init__(self):
+    super
+    pass
+
+class Starship(FlightVehicle):
+  def __init__(self):
+    super
+    pass
+
+class Airplane(FlightVehicle):
+  def __init__(self):
+    super
+    pass
+
+class GroundVehicle(Vehicle):
+  def __init__(self):
+    super
+    pass
+
+class Car(GroundVehicle):
+  def __init__(self):
+    super
+    pass
+
+class Motorcycle(GroundVehicle):
+  def __init__(self):
+    super
+    pass
 # the hierarchy.
 #
 # e.g.
